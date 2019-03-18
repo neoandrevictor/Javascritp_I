999633083605d996eccf29f32ef6d3a890012312
patricspires <patric.pires@outlook.com>
2019-03-18 18:00:16 -0300

    use $ to create an aliases to querySelector

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,7 +2,16 @@ class NegociacaoController {
 
   adiciona(event){
     event.preventDefault();
-    alert(`Chamei acao no controller`)
+
+    let $ = document.querySelector.bind(document);
+
+    let inputData = $('#data')
+    let inputQuantidade = $('#quantidade')
+    let inputValor= $('#valor')
+
+    console.log(inputData.value)
+    console.log(inputQuantidade.value)
+    console.log(inputValor.value)
   }
 
 }
