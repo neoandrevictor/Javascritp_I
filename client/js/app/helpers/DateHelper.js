d23e114d30b33982a28c05120d17562bd5cf8126
patricspires <patric.pires@outlook.com>
2019-03-22 17:51:12 -0300

    use regex to validade input of date

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -9,6 +9,8 @@ class DateHelper {
   }
 
   static textoParaData(texto){
+
+    if(!/\d{4}-\d{2}-\d{2}/.test(texto)) throw new Error('Deve estar no formato Ano-Mes-Dia')
     return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
   }
 }
